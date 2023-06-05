37fb4018d64ef1701e7f043ab57aeb4b1d45c074
Kaleb <KalebStephens122@gmail.com>
2023-06-05 11:36:12 -0500

    Fetched Url in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 
 const COHORT_NAME = "2303-FTB-ET-WEB-AM";
-const BASE_URL = " https://strangers-things.herokuapp.com/${COHORT_NAME}";
-const returned = await fetch(`${BASE_URL}/posts`);
-
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
 // So, for example, to call for all posts the url would just need to be:
 // `${BASE_URL}/posts`
+
+async function fetchUrl() {
+  const response = await fetch(`${BASE_URL}/posts`);
+  // waits until the request completes...
+  console.log(response);
+}
+console.log(fetchUrl());
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -6,19 +6,21 @@ import App from "./App";
 import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-const Apps = () => {
-  const [posts, setPosts] = useState([]);
+const COHORT_NAME = "2303-FTB-ET-WEB-AM";
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
 
-  useEffect(() => {}, []);
+const Apps = () => {
+  //   const [posts, setPosts] = useState([]);
+  //   useEffect(() => {}, []);
   return (
     <div>
       <h1>Strangers Things</h1>
-      <nav>
+      {/* <nav>
         <Link to="/posts">Posts ({posts.length})</Link>
         <Link to="/login">Login</Link>
         <Link to="/register">Register</Link>
       </nav>
-      {/* <Routes>
+      <Routes>
         <Route path="/posts" element={<div>Posts</div>} />} />
         <Route path="/login" element={<div>Login</div>} />} />
         <Route path="/register" element={<div>Register</div>} />} />
@@ -26,6 +28,13 @@ const Apps = () => {
     </div>
   );
 };
+// async function fetchUrl() {
+//   const response = await fetch(`${BASE_URL}/posts`);
+//   // waits until the request completes...
+//   console.log(response);
+// }
+
+// export default fetchUrl;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
