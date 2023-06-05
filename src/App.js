c60e00e5483d2cd6fde7c61d18ff8658ad91e9b3
Kaleb <KalebStephens122@gmail.com>
2023-06-05 09:50:19 -0500

    Added boiler plate. (dependencies, imports, rtrn)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
 
+const COHORT_NAME = "2303-FTB-ET-WEB-AM";
+const BASE_URL = " https://strangers-things.herokuapp.com/${COHORT_NAME}";
+
 function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -3,5 +3,29 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+const Apps = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {}, []);
+  return (
+    <div>
+      <h1>Strangers Things</h1>
+      <nav>
+        <Link to="/posts">Posts ({posts.length})</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </nav>
+      {/* <Routes>
+        <Route path="/posts" element={<div>Posts</div>} />} />
+        <Route path="/login" element={<div>Login</div>} />} />
+        <Route path="/register" element={<div>Register</div>} />} />
+      </Routes> */}
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
