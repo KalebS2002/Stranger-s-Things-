5c121f90f3576450af0318f054bad0c4d2293633
Kaleb <KalebStephens122@gmail.com>
2023-06-05 10:04:00 -0500

    Updated File Structure. Added Components.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import "./App.css";
 
 const COHORT_NAME = "2303-FTB-ET-WEB-AM";
 const BASE_URL = " https://strangers-things.herokuapp.com/${COHORT_NAME}";
+const returned = await fetch(`${BASE_URL}/posts`);
+
+// So, for example, to call for all posts the url would just need to be:
+// `${BASE_URL}/posts`
 
 function App() {
   return (
