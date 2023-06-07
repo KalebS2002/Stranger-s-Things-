aabe419db7034b7daf3ce5152151d875e6f0208a
Kaleb <KalebStephens122@gmail.com>
2023-06-07 09:56:27 -0500

    Rendered and styled Post data from api. Sucsess!

--- src/Components/Apps.js
+++ src/Components/Apps.js
@@ -3,23 +3,48 @@ import ReactDOM from "react-dom/client";
 import NavBar from "./NavBar";
 import Login from "./Login";
 import Post from "./Posts";
+import Register from "./Register";
+import Profile from "./Profile";
 import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-
-const posts = () => {};
+const COHORT_NAME = "2303-FTB-ET-WEB-AM";
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
 
 const Apps = () => {
-  const [posts, setPosts] = useState([]);
   useEffect(() => {}, []);
+  const [posts, setPosts] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     try {
+  //       const response = await fetch(`${BASE_URL}/posts`);
+  //       const result = await response.json();
+  //       const postData = result.data.posts;
+  //       console.log(result, postData);
+
+  //       setPosts(postData);
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   };
+  //   fetchPosts();
+  // }, []);
 
   return (
-    <div>
+    <>
       <NavBar />
-      <div>
-        {/* <Login /> */}
-        {/* <Posts /> */}
-      </div>
-    </div>
+      {/* <div>
+        <div className="allPosts">
+          {posts.map((post) => (
+            <div key={post._id}>
+              Title: {post.title}
+              Location: {post.location}
+              Id: {post.id}
+            </div>
+          ))}
+        </div>
+      </div> */}
+    </>
   );
 };
 
